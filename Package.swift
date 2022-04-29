a3d4f8ce1b92b01603c013bc02edfd90016628dd
Alexander Schiewe <alexander.schiewe@img.ly>
2022-04-29 15:22:17 +0200

    Prepare version 11.0.0

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
   name: "PhotoEditorSDK",
-  platforms: [.iOS(.v9)],
+  platforms: [.iOS(.v13)],
   products: [
     .library(name: "PhotoEditorSDK", targets: ["PhotoEditorSDK"]),
   ],
